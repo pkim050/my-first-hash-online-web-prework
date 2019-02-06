c2b072d599a11ff71b71f8cb6fee0f8f36733805
Patrick Kim <pkim050@gmail.com>
2019-02-06 22:19:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,6 +9,7 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {"whale bone corsets": 5, "porcelain vases": 2, "oil paintings": 3}
+  return the_manifest
 end
 
 def retrieval
@@ -19,7 +20,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
+  return shipping_manifest
 end
 
 def adding
@@ -29,13 +31,9 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
+  shipping_manifest["muskets"] = 2
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
+  shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
-
+  return shipping_manifest
 end
